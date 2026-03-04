8b8a2f13016ad9aced9552f61c143ac98aa315ab
SashaSaw <sawsasha26@gmail.com>
2026-03-04 14:48:29 +0000

    Initial Commit

---
+++ Squidget/SquidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquidgetApp.swift
+//  Squidget
+//
+//  Created by Alexander Saw on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SquidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
